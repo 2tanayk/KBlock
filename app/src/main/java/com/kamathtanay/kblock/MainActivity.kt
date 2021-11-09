6485501730dd61271643054ce92387a0f561da77
Tanay Kamath <2tanaykamath@gmail.com>
2021-11-09 20:40:27 +0530

    initial commit

---
+++ app/src/main/java/com/kamathtanay/kblock/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kamathtanay.kblock
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
